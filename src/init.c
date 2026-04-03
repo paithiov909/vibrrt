eb046369dba59ebd8cba231d895e3b209b40a556
paithiov909 <am11.44@gmail.com>
2026-04-03 21:18:25 +0900

    chore: cargo update

--- src/init.c
+++ src/init.c
@@ -1,7 +1,12 @@
 
+// clang-format sorts includes unless SortIncludes: Never. However, the ordering
+// does matter here. So, we need to disable clang-format for safety.
+
+// clang-format off
 #include <stdint.h>
 #include <Rinternals.h>
 #include <R_ext/Parse.h>
+// clang-format on
 
 #include "rust/api.h"
 
@@ -49,6 +54,6 @@ void R_init_vibrrt(DllInfo *dll) {
     R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
     R_useDynamicSymbols(dll, FALSE);
 
-    // Functions for initialzation, if any.
+    // Functions for initialization, if any.
 
 }
